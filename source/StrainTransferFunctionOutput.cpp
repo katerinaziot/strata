df308a839ebcf48ce0df30e61cb8adcc7756d2e8
Katerina Ziotopoulou <kziotopoulou@ucdavis.edu>
2025-04-22 16:37:17 -0700

    Update StrainTransferFunctionOutput.cpp

--- source/StrainTransferFunctionOutput.cpp
+++ source/StrainTransferFunctionOutput.cpp
@@ -30,7 +30,7 @@
 #include "OutputCatalog.h"
 #include "SoilProfile.h"
 
-#include <qwt_scale_engine.h>
+#include <QwtScaleEngine>
 
 StrainTransferFunctionOutput::StrainTransferFunctionOutput(
     OutputCatalog *catalog)
